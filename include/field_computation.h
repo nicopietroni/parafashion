ac6172f8d8b0f859fb30ec639c453d4518a2c6ca
CorentinDumery <>
2021-12-13 12:27:29 +0100

    Compilation fix

--- include/field_computation.h
+++ include/field_computation.h
@@ -30,44 +30,7 @@ public:
     static void ComputeField(TriMeshType &mesh,
                              FieldMode FMode,
                              ScalarType CurvatureFidelity,
-                             bool DebugMsg=false)
-    {
-        typedef FieldSmoother<TriMeshType> FieldSmootherType;
-        typename FieldSmootherType::SmoothParam Param;
-        Param.align_borders=true;
-        if (FMode==FMBoundary)
-        {
-            //Param.alpha_curv=CurvatureFidelity;
-            Param.SmoothM=SMNPoly;
-        }
-        else
-        {
-            Param.alpha_curv=CurvatureFidelity;
-            Param.SmoothM=SMMiq;
-        }
-
-        //add features
-        size_t NumFeatures=0;
-        for (size_t i=0;i<mesh.face.size();i++)
-        {
-            for (size_t j=0;j<3;j++)
-            {
-                if (!mesh.face[i].IsFaceEdgeS(j))continue;
-                CoordType Dir=mesh.face[i].P0(j)-mesh.face[i].P1(j);
-                Dir.Normalize();
-                Param.AddConstr.push_back(std::pair<int,CoordType>(i,Dir));
-                NumFeatures++;
-                break;//one constraint per face
-            }
-        }
-        if (DebugMsg)
-            std::cout<<"Added "<<NumFeatures<<" features"<<std::endl;
-
-        FieldSmootherType::SmoothDirections(mesh,Param);
-
-        vcg::tri::CrossField<TriMeshType>::UpdateSingularByCross(mesh);
-        vcg::tri::CrossField<TriMeshType>::SetVertCrossVectorFromFace(mesh);
-    }
+                             bool DebugMsg=false);
 
 };
 
